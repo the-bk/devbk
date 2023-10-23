269ecb333a10f1932bdc8ac327598489a25b1a89
Balkrishna Londhe <balkrishna.londhe@zs.com>
2023-10-23 13:59:04 +0530

    Updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -27,27 +27,63 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 
 version = "2023.05"
 
+// project {
+//     val bts = sequential {
+//         buildType(Maven("Build","clean compile"))
+//         parrallel{
+//             buildType(Maven("Fast Test","clean test","-Dmaven.test.failure.ignore=true -Dtest=*.unit.*Test"))
+//             buildType(Maven("Slow Test","clean test","-Dmaven.test.failure.ignore=true -Dtest=*.integration.*Test"))
+//         }
+//         buildType(Maven("Package","clean package","-Dmaven.test.failure.ignore=true"))
+//     }.buildSteps()
+
+//     bts.forEach{ buildType(it) }
+//     bts.last().triggers {
+//         vcs {
+
+//         }
+//     }
+// }
+
+// class Maven(name: String, goals: Strings, runnerArgs: String? = null) : BuildType ({
+//     id(name.toExtId())
+//     this.name = name
+
+//     vcs {
+//         root(DslContext.settingsRoot)
+//     }
+
+//     steps {
+//         maven {
+//             this.goals = goals
+//             this.runnerArgs = runnerArgs
+//         }
+//     }    
+// })
+
 project {
-    val bts = sequential {
-        buildType(Maven("Build","clean compile"))
+
+    buildType(Build)
+    buildType(FastTest)
+    buildType(SlowTest)
+    buildType(Package)
+
+    sequential {
+        buildType(Build)
         parrallel{
-            buildType(Maven("Fast Test","clean test","-Dmaven.test.failure.ignore=true -Dtest=*.unit.*Test"))
-            buildType(Maven("Slow Test","clean test","-Dmaven.test.failure.ignore=true -Dtest=*.integration.*Test"))
+            buildType(FastTest)
+            buildType(SlowTest)
         }
-        buildType(Maven("Package","clean package","-Dmaven.test.failure.ignore=true"))
-    }.buildSteps()
-
-    bts.forEach{ buildType(it) }
-    bts.last().triggers {
-        vcs {
-
-        }
+        // parrallel {
+        //     buildType(FastTest)
+        //     buildType(SlowTest)
+        // }
+        buildType(Package)
     }
 }
 
-class Maven(name: String, goals: Strings, runnerArgs: String? = null) : BuildType ({
-    id(name.toExtId())
-    this.name = name
+object Build : BuildType({
+    name = "Build"
 
     vcs {
         root(DslContext.settingsRoot)
@@ -55,8 +91,58 @@ class Maven(name: String, goals: Strings, runnerArgs: String? = null) : BuildTyp
 
     steps {
         maven {
-            this.goals = goals
-            this.runnerArgs = runnerArgs
+            goals = "clean compile"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
-    }    
+    }
+})
+
+object FastTest : BuildType({
+    name = "Fast Test"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true -Dtest=*.unit.*Test"
+        }
+    }
+})
+
+object SlowTest : BuildType({
+    name = "Slow Test"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true -Dtest=*.integration.*Test"
+        }
+    }
+})
+
+object Package : BuildType({
+    name = "Package"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            goals = "clean package"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
 })
